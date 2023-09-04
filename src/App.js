ec51179237c9d858514eceffed637c28a6661397
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-09-05 01:12:12 +0530

    tailwind and load the assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-function App() {
-  return <div>HI</div>;
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="text-4xl  font-signature ">HarshaShetty </h1>
+      <h2>Heloo</h2>
+    </div>
+  );
 }
-
-export default App;
